0362904d81ce13c015ef871116738f08b0ecd430
Stephen Blackstone <sblackstone@gmail.com>
2015-04-08 18:41:21 -0400

    Running the TM now works internally

--- beaver.js
+++ beaver.js
@@ -27,6 +27,17 @@ TapeNode.prototype.get_right = function() {
 
 var Tape = function() {
 	this.current = new TapeNode();		
+  this.ones_count = 0;
+}
+
+Tape.prototype.set_value = function(n) {
+	if (this.current.value == 1 && n == 0) {
+		this.ones_count--;
+	}
+	if (this.current.value == 0 && n == 1) {
+		this.ones_count++;
+	}
+	this.current.value = n;
 }
 
 Tape.prototype.move_left = function() {
@@ -47,23 +58,35 @@ angular.module('beaver-app', [])
     beaver.state_count = 2;
     beaver.tape = null;
     beaver.current_state = null;
-
+    beaver.loop_speed = 250;
+    beaver.step_count = 0;
+    beaver.state_chars = {
+	    0: 'A',
+	    1: 'B',
+	    2: 'C',
+	    3: 'D',
+	    4: 'E',
+	    'H': 'H',
+	
+   };
     beaver.run = function() {
 	    beaver.current_state = beaver.states[0];
+	    beaver.step_count = 0;
       beaver.tape          = new Tape();
-      beaver.loop_timer = window.setTimeout(beaver.one_loop, 250);
+      beaver.loop_timer = window.setTimeout(beaver.one_loop, 1);
     }
 
     beaver.one_loop = function() {
+	      beaver.step_count++;
+	      console.log("Step = " + beaver.step_count);
 		    console.log("Executing state " + beaver.current_state.id);
 		    var cur_val = beaver.tape.current.value;
 		    console.log('Read a ' + cur_val + " at position " + beaver.tape.current.node_id);
         var ins = beaver.current_state.instructions[cur_val];
-        console.log("instructions = ");
-        console.log(ins);
+        console.log("instructions: write a " + ins.write + ", move " + ins.move + " and goto state " + ins.next);
 
         console.log('Writing a ' + ins.write + " to node " +  beaver.tape.current.node_id);
-        beaver.tape.current.value = parseInt(ins.write);
+        beaver.tape.set_value(parseInt(ins.write));
 
         if (ins.move == 'L') {
  	        beaver.tape.move_left();
@@ -76,13 +99,14 @@ angular.module('beaver-app', [])
 
        if (ins.next == 'H') {
          console.log('HALTING!');
+         console.log('Total ones on tape is ' + beaver.tape.ones_count);
          return;
        } else {
 	       console.log("Setting current state to " + ins.next);
 	       beaver.current_state = beaver.states[parseInt(ins.next)];
        }
        console.log("------");		
-       beaver.loop_timer = window.setTimeout(beaver.one_loop, 1000);
+       beaver.loop_timer = window.setTimeout(beaver.one_loop, beaver.loop_speed );
     }
 
     beaver.next_values = function() {
